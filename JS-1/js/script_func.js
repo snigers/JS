00fd02d64599b134256f8c3648bde483c9fa59e3
snigers <educon@yandex.ru>
2019-04-23 07:57:50 +0300

    style js

--- JS-1/js/script_func.js
+++ JS-1/js/script_func.js
@@ -1,37 +1,37 @@
-var userName = 'Вася';
+// var userName = 'Вася';
 
-function showMessage(from, text) {
-  if (text === undefined) {
-    text = 'Текст не передан';
-  }
-  from = "** " + from + " **";
+// function showMessage(from, text) {
+//   if (text === undefined) {
+//     text = 'Текст не передан';
+//   }
+//   from = "** " + from + " **";
   
-  alert(from + ': ' + text);
-}
+//   alert(from + ': ' + text);
+// }
 
-function count() {
-  for (var i = 0; i < 3; i++) {
-    var j = i * 2;
-  }
-  alert(i);
-  alert(j);
-}
+// function count() {
+//   for (var i = 0; i < 3; i++) {
+//     var j = i * 2;
+//   }
+//   alert(i);
+//   alert(j);
+// }
 
-function calcD(a, b, c) {
-  return b * b - 4 * a * c;
-}
+// function calcD(a, b, c) {
+//   return b * b - 4 * a * c;
+// }
 
-var test = calcD(-4, 2, 1);
+// var test = calcD(-4, 2, 1);
 // alert(test);
 
-function chackAge(age) {
+// function chackAge(age) {
 /*   if (age > 18) {
     return true;
   } else {
     return confirm('Родители разрешили?');
   } */
-  return (age > 18) || confirm('Родители разрешили?');
-}
+//   return (age > 18) || confirm('Родители разрешили?');
+// }
 
 // var age = prompt('Ваш возраст?');
 
@@ -47,22 +47,22 @@ function chackAge(age) {
 // var a = prompt('a');
 // var b = prompt('b');
 // alert(pow(a,b));
-function pow(a, b) {
-  var num = a;
+// function pow(a, b) {
+//   var num = a;
 
-  if (b == 0) {
-    num = a * 0;
-  } else if (b == 1) {
-    num = a;
-  } else if (b < 0) {
-    num = 'Пока не могу вычислить :(';
-  } else {
-    for (var i = 1; i < b; i++) {
-      num = num * a;
-    }
-  }
-  return num;
-}
+//   if (b == 0) {
+//     num = a * 0;
+//   } else if (b == 1) {
+//     num = a;
+//   } else if (b < 0) {
+//     num = 'Пока не могу вычислить :(';
+//   } else {
+//     for (var i = 1; i < b; i++) {
+//       num = num * a;
+//     }
+//   }
+//   return num;
+// }
 
 // var age = prompt("Сколько вам лет?");
 
@@ -72,10 +72,10 @@ function pow(a, b) {
 
 // sayHi();
 
-function ask(question, yes, no) {
-  if (confirm(question)) yes()
-  else no();
-}
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes()
+//   else no();
+// }
 
 // function showOk() {
 //   alert('Вы согласились.');
@@ -91,14 +91,47 @@ function ask(question, yes, no) {
 //   function() {alert('Вы отказались.');}
 // );
 
-function sumTo(n) {
-  var result = 0;
-  for (var i = 1; i <= n; i++) {
+// function sumTo(n) {
+//   var result = 0;
+//   for (var i = 1; i <= n; i++) {
     
-    result += i; 
-  }
-  return result;
-}
+//     result += i; 
+//   }
+//   return result;
+// }
 
-alert(sumTo(100));
+// alert(sumTo(100));
+
+// var f = function sayHi(name) {
+//   alert(sayHi);
+// };
+
+// alert(f);
+
+// var test = function sayHi(name) {
+//   sayHi = "Тест";
+//   alert(sayHi);
+// };
+
+// test();
+
+// Функция факториала
+// function f(n) {
+//   return n ? n * f(n - 1) : 1;
+// };
+
+// var g = f;
+// f = null;
+
+// alert(g(5)); //Ошибка при выполнение
+
+//Чтобы функция всегда работала, надо так:                                     
+/* var f = function factorial(n) {
+  return n ? n * factorial(n - 1) : 1;
+};
+
+var g = f;
+f = null;
+
+alert(g(5)); */ //работает
 
